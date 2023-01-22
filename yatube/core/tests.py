b7637c53702f0a6055399ab2d68252b2e0c13404
Ivan Korbakov <korbakovi@gmail.com>
2023-01-22 20:48:15 +0600

    add test 404.html

--- yatube/core/tests.py
+++ yatube/core/tests.py
@@ -1,8 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.cache import cache
 
 
 class ViewTestClass(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def tearDown(self):
+        cache.clear()
+
     def test_error_page(self):
-        response = self.client.get('/nonexist-page/')
-        # Проверьте, что статус ответа сервера - 404
-        # Проверьте, что используется шаблон core/404.html
+        response = self.guest_client.get('/nonexist-page/')
+        self.assertTemplateUsed(response, 'core/404.html')
